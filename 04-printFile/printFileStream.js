f958d85256cac27d1ee43bb1530b43d9e8fe2f43
Magesh <tkmagesh77@gmail.com>
2019-02-13 15:43:45 +1300

    read file stream

---
+++ 04-printFile/printFileStream.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+var stream = fs.createReadStream('./sample.txt', {encoding : 'utf8'});
+
+//open, data, end, close, error
+var readCount = 0;
+
+stream.on('data', function(chunk){
+    console.log(chunk);
+    ++readCount;
+});
+
+stream.on('end', function(){
+    console.log('thats all mate!');
+    console.log('readCount = ', readCount);
+});
+
+stream.on('error', function(err){
+    console.log(err);
+});
