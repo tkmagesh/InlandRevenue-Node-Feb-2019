3def765944a6ed4545cfc3192886e156a0846578
Magesh <tkmagesh77@gmail.com>
2019-02-15 14:29:10 +1300

    tasks routes complete

--- 10-task-manager/routes/tasksRoutes.js
+++ 10-task-manager/routes/tasksRoutes.js
@@ -11,4 +11,56 @@ router.get('/', function(req, res, next){
     res.json(taskList);
 });
 
+router.get('/:id', function(req, res, next){
+    var resultTask = taskList.find(function(task){
+        return task.id === parseInt(req.params.id);
+    });
+    if (!resultTask){
+        res.status(404).end();
+    } else {
+        res.json(resultTask);
+    }
+});
+
+router.post('/', function(req, res, next){
+    var postedTask = req.body;
+    taskList.push(postedTask);
+    res.status(201).json(postedTask);
+});
+
+router.put('/:id', function(req, res, next){
+    var updatedTask = req.body,
+        updatedTaskId = parseInt(req.params.id);
+
+    var taskToUpdate = taskList.find(function(task){
+        return task.id === updatedTaskId;
+    });
+
+    if (taskToUpdate){
+        taskList = taskList.map(function(task){
+            return task.id === updatedTaskId ? updatedTask : task;
+        });
+        return res.json(updatedTask);
+    } else {
+        res.status(404).end();
+    }
+});
+
+router.delete('/:id', function(req, res, next){
+    var deletedTaskId = parseInt(req.params.id);
+
+    var taskToDelete = taskList.find(function(task){
+        return task.id === deletedTaskId;
+    });
+
+    if (taskToDelete){
+        taskList = taskList.filter(function(task){
+            return task.id !== deletedTaskId;
+        });
+        return res.json({});
+    } else {
+        res.status(404).end();
+    }
+})
+
 module.exports = router;
